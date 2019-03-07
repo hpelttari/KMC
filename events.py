7a48b7dc9c86c891c88a625d5446e0af8fe9bf7a
Hannu <hannu.pelttari@gmail.com>
2019-03-07 20:59:53 +0200

    Add desorption and diffusion events

--- events.py
+++ events.py
@@ -1,4 +1,15 @@
 
 def adsorption(grid, x, y):
-    grid[x][y]=grid[x][y]+1
+    grid[x][y] = grid[x][y] + 1
+    return grid
+
+
+def desorption(grid, x, y):
+    grid[x][y] = grid[x][y] - 1
+    return grid
+
+
+def diffusion(grid, x, y, x2, y2):
+    grid[x][y] = grid[x][y] - 1
+    grid[x2][y2] = grid[x2][y2] + 1
     return grid
