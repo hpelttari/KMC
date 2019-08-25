c02ee9df0bb756d4ff716f558f6655292f8142d6
Hannu <hannu.pelttari@gmail.com>
2019-08-25 14:23:48 +0300

    Add functions to calculate the event probabilities

--- events.py
+++ events.py
@@ -13,3 +13,22 @@ def diffusion(grid, x, y, x2, y2):
     grid[x][y] = grid[x][y] - 1
     grid[x2][y2] = grid[x2][y2] + 1
     return grid
+
+
+def calculate_diffusion_probability(grid, x, y, x2, y2, p):
+
+    # diffusion is only possible to the same or lower level
+    # as the diffusing atom
+    if grid[x2][y2] < grid[x][y]:
+        return p
+
+    return 0
+
+
+def calculate_desorption_probability(grid, x, y, p):
+
+    # Desorption is only possible for the adatoms
+    if grid[x][y]>0:
+        return p
+
+    return 0
